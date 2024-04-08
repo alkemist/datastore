6d2a618af1e8f61df002712fe42b8a7ad35c324d
Jaden ACHAIN <jaden.achain@gmail.com>
2024-04-08 11:24:45 +0200

    Fixes & Improves

--- public/index.php
+++ public/index.php
@@ -5,5 +5,15 @@ use App\Kernel;
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
 return function (array $context) {
-    return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
+    $env = $context['APP_ENV'];
+    $debug = (bool)$context['APP_DEBUG'];
+    $devIp = $context['APP_DEV_IP'];
+    $ignoreIp = (bool)$context['APP_IGNORE_IP'];
+
+    if (!$ignoreIp && $_SERVER['REMOTE_ADDR'] === $devIp) {
+        $env = 'dev';
+        $debug = true;
+    }
+
+    return new Kernel($env, $debug);
 };
